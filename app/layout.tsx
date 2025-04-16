434c82030c256f1b70eb9c42b48957ba8393b74b
mgs28-mh <gsaputra880@gmail.com>
2025-04-16 21:59:41 +0700

    mgs: suppress Hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={`bg-[#FCFCFC] dark:bg-black ${poppins.className}`}>
         {children}
         <Providers>
