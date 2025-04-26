9cf368f27689a6132541ae22374766698b669db3
mgs28-mh <gsaputra880@gmail.com>
2025-04-26 16:40:41 +0700

    mgs: remove unused icons

--- components/feature.tsx
+++ components/feature.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { FaUsers, FaCalendarAlt, FaBookOpen, FaLeaf, FaLightbulb, FaHandsHelping } from "react-icons/fa";
+import { FaUsers, FaLightbulb, FaHandsHelping } from "react-icons/fa";
 
 export default function Features() {
   return (
